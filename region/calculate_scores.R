0ea59cb611ffa5906bbd0f84e752beffc0d33023
jules32 <jstewart@nceas.ucsb.edu>
2018-07-31 13:17:52 -0700

    modernize calculate_scores.R

--- region/calculate_scores.R
+++ region/calculate_scores.R
@@ -4,14 +4,15 @@
 ## - configure_toolbox.r ensures your files are properly configured for `ohicore`.
 ## - The `ohicore` function CalculateAll() calculates OHI scores.
 
-## set working directory for all OHI calculations ----
-setwd("~/github/ne-scores/region2016")
-
 ## load required packages. If not installed, install from the comment ----
 library(ohicore)   # source('~/github/ohi-northeast/install_ohicore.R')
 library(tidyverse) # install.packages('tidyverse')
+library(here)      # install.packages('here')
 library(stringr)   # install.packages('stringr')
 library(zoo)       # install.packages('zoo')
+
+## set working directory for all OHI calculations ----
+setwd(here::here("region"))
 
 
 ## calculate scores for each year scenario and save to a single csv file ----
@@ -22,21 +23,21 @@ scores_all_years <- data.frame()
 
 ## loop through each scenario year
 for (s_year in scenario_years){  # s_year=2015
- 
+
   message(sprintf('--- Calculating Scores for scenario year %s ----', s_year))
-  
+
   ## configure checks
   conf   <-  ohicore::Conf('conf')
   layers <-  ohicore::Layers(layers.csv = 'layers.csv', layers.dir = 'layers')
-  layers$data$scenario_year <-  s_year 
-  
+  layers$data$scenario_year <-  s_year
+
   ## calculate scores
   scores_sy <- ohicore::CalculateAll(conf, layers) %>%
-    mutate(year = s_year) 
-  
+    mutate(year = s_year)
+
   ## bind scores to dataframe
   scores_all_years <- rbind(scores_all_years, scores_sy)
-  
+
 }
 
 ## save .csv file
