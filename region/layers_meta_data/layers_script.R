ba562b33171a12bbe5f384bebcfddd8cacb3a109
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2019-09-20 13:18:38 -0700

    updated toolbox with the new layers_meta_data folder. also updated resilience and pressures matrices. i think this might be the first run with all the resilience layers from juliette

---
+++ region/layers_meta_data/layers_script.R
@@ -0,0 +1,30 @@
+
+#####################################
+## This code takes some of the
+## information in this file and
+## creates a layers.csv file in the
+## format used by the toolbox
+## This is sourced by the region/calculate_scores.Rmd file
+#####################################
+
+
+targets <- read.csv('layers_meta_data/layers_targets.csv', stringsAsFactors=FALSE) %>%
+  dplyr::mutate(dimension = ifelse(dimension %in% c("status", "trend"), NA, dimension)) %>%
+  dplyr::filter(!is.na(dimension) | !is.na(goal)) %>%
+  dplyr::mutate(goal = ifelse(is.na(goal), dimension, goal)) %>%
+  dplyr::mutate(target = paste(goal, dimension, sep=' ')) %>%
+  dplyr::mutate(target = gsub(" NA", "", target)) %>%
+  unique() %>%
+  dplyr::group_by(layer) %>%
+  dplyr::summarize(targets = paste(target, collapse= "")) %>%
+  data.frame()
+
+
+# add the meta data
+meta <- read.csv("layers_meta_data/layers_base.csv")
+layers <- meta %>%
+  dplyr::left_join(targets, by="layer") %>%
+  dplyr::select(layer, filename, fld_value, targets, name, units, description)
+
+
+write.csv(layers, here("region/layers.csv"), row.names=FALSE)
